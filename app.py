1c8267bf4a7ad3b5a0b40dfcd81f9b30826d414f
Jimmy Le <le.jimmy91@gmail.com>
2020-03-25 16:44:21 -0500

    added total confirmed and total deaths

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import dash_html_components as html
 
 import pandas as pd
 
+import plotly.express as px
+
 # initialize app
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -16,16 +18,21 @@ server = app.server
 
 # load data
 
-df_confirmed = pd.read_csv('https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-df_deaths = pd.read_csv('https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+df_confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+df_deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
 
-df_total = pd.DataFrame(df.iloc[:, 4:].sum()).reset_index().rename(columns = {'index': 'Date', 0: 'Confirmed'})
+df_tc = pd.DataFrame(df_confirmed.iloc[:, 4:].sum()).reset_index().rename(columns = {'index': 'Date', 0: 'Confirmed'})
+df_td = pd.DataFrame(df_confirmed.iloc[:, 4:].sum()).reset_index().rename(columns = {'index': 'Date', 0: 'Deaths'})
+
 
 # app layout
 
-graph = dcc.Graph(
-            id = 'selected-datate',
-            figure = px.line( data_frame = df_total, x = 'Date', y = 'Confirmed' ))
+graph_c = html.Div([dcc.Graph(id = 'graph_confirmed',
+                                      figure = px.line( data_frame = df_tc, x = 'Date', y = 'Confirmed' ))])
+
+graph_d = html.Div([dcc.Graph(id = 'graph_deaths',
+                                      figure = px.line( data_frame = df_td, x = 'Date', y = 'Deaths' ))])
+
 
 app.layout = html.Div([
     html.H2('Hello World'),
@@ -35,7 +42,8 @@ app.layout = html.Div([
         value='LA'
     ),
     html.Div(id='display-value'),
-    graph
+    graph_c,
+    graph_d
 ])
 
 # callbacks
